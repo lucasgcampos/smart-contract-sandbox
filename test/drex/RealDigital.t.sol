867d61d8c1f14665acf2e6c547df5997cf6edf6c
Lucas Campos <lucasgcampos.contato@gmail.com>
2025-01-24 19:26:27 -0300

    Initial test to RealDigital

---
+++ test/drex/RealDigital.t.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {RealDigital} from "../../src/drex/RealDigital.sol";
+
+contract RealDigitalTest is Test {
+    
+    RealDigital realDigital;
+    address deployer = address(0xff);
+
+    function setUp() public {
+        vm.prank(deployer);
+        realDigital = new RealDigital();
+    }
+
+    function test_inital_balance_of_deployer() public {
+        assertEq(realDigital.balanceOf(deployer), 1000 * 100);
+    }
+}
