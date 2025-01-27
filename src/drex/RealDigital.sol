81580c30de52d5382bc594632c6ead4e3c115dda
Lucas Campos <lucasgcampos.contato@gmail.com>
2025-01-26 22:20:00 -0300

    RealDigital integrating with STR

--- src/drex/RealDigital.sol
+++ src/drex/RealDigital.sol
@@ -4,8 +4,24 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract RealDigital is ERC20 {
-    constructor() ERC20("Real Digital", "RD") {
-        _mint(msg.sender, 1000 * 100);
+
+    address str;
+
+    constructor(address _str) ERC20("Real Digital", "RD") {
+        str = _str;
+    }
+
+    modifier onlySTR {
+        require(msg.sender == str, "Unauthorized");
+        _;
+    }
+
+    function mint(address _participant, uint _amount) external onlySTR {
+        _mint(_participant, _amount);
+    }
+
+    function burnFrom(address _participant, uint _amount) external onlySTR {
+        _burn(_participant, _amount);
     }
 
     function decimals() public view virtual override returns (uint8) {
