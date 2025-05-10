532c45be5161d6a2bc3ded8ea46e7a515d46fda5
Lucas Campos <lucasgcampos.contato@gmail.com>
2025-05-10 16:23:43 -0300

    Fix swap contract

--- src/drex/SwapOneStep.sol
+++ src/drex/SwapOneStep.sol
@@ -7,8 +7,11 @@ import {RealTokenizado} from "./RealTokenizado.sol";
 contract SwapOneStep {
 
     address[] participants;
+    RealDigital public realDigital;
 
     constructor(address _realDigitalContract, address[] memory _participants) {
+        realDigital = RealDigital(_realDigitalContract);
+
         for (uint i = 0; i < _participants.length; i++) {
             participants.push(_participants[i]);
         }
@@ -27,7 +30,6 @@ contract SwapOneStep {
     }
 
     function swap(
-        address _realDigital,
         address _fromRealTokenizado,
         address _fromClient,
         address _toRealTokenizado,
@@ -38,8 +40,7 @@ contract SwapOneStep {
         RealTokenizado fromToken = RealTokenizado(_fromRealTokenizado);
         require(fromToken.balanceOf(_fromClient) >= _amount, "Unsufficient balance");
         
-        // Transfer Real Digital
-        RealDigital realDigital = RealDigital(_realDigital);
+        // Transfer Real Digital        
         realDigital.transferSwap(fromToken.owner(), toToken.owner(), _amount);
 
         // Swap Tokens
